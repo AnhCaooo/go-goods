1ca2b174a2603b424a4cfb9f9bdc2611412243e9
Anh Cao <anhcao4922@gmail.com>
2024-11-27 23:18:48 +0200

    feat: implement logger
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- log/log.go
+++ log/log.go
@@ -1,1 +1,40 @@
+// AnhCao 2024
 package log
+
+import (
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+/*
+initialize and return logger instance (unstructured log)
+
+*Note*: it is recommended to buffered log entries are flushed before the program exits.
+
+Example usage:
+
+	Logger := log.InitLogger(zapcore.InfoLevel)
+	defer Logger.Sync()
+*/
+func InitLogger(level zapcore.Level) *zap.Logger {
+	// todo: do we want to store the log in specific file for investigation purposes?
+	cfg := zap.NewProductionConfig()
+	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	cfg.EncoderConfig.EncodeTime = syslogTimeEncoder
+	// Set log level
+	cfg.Level.SetLevel(zapcore.Level(level))
+	// Disable JSON encoding
+	cfg.Encoding = "console"
+
+	logger, err := cfg.Build()
+	if err != nil {
+		panic(err)
+	}
+	return logger
+}
+
+func syslogTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format("2006-01-02 15:04:05"))
+}
