c39e7b739d6d0eadd09adab8a1e73ccb0970ec99
Anh Cao <anhcao4922@gmail.com>
2024-12-02 17:26:24 +0200

    feat: add docs comment for auth package

--- auth/auth.go
+++ auth/auth.go
@@ -7,7 +7,24 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-// receive token and JWT secret key as string to do verify the token
+// VerifyToken verifies the authenticity of a JWT token using the provided secret key.
+//
+// This function parses the provided `tokenString`, verifies its signature using the provided
+// `secretKey`, and checks the token's validity. If the token is invalid or the verification
+// fails, an error is returned.
+//
+// EXAMPLE USAGE:
+//
+//	if err := auth.VerifyToken(tokenString, secretKey); err != nil {
+//	    return err
+//	}
+//
+// PARAMETERS:
+//   - tokenString: The JWT token string to be verified.
+//   - secretKey: The SECRET KEY used to verify the token's signature.
+//
+// RETURNS:
+//   - error: An ERROR if the token cannot be parsed or is invalid; nil otherwise.
 func VerifyToken(tokenString, secretKey string) error {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		return []byte(secretKey), nil
