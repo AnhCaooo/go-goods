38606118bd052c0df22db9ab05cc9d15ef3401c6
Anh Cao <anhcao4922@gmail.com>
2025-01-04 10:36:55 +0200

    feat: implement func to map interface to specific struct

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -22,4 +23,23 @@ func TrimSpaceForByte(value []byte) []byte {
 	strVal := string(value)
 	trimmedStrVal := strings.TrimSpace(strVal)
 	return []byte(trimmedStrVal)
+}
+
+// MapInterfaceToStruct converts an interface{} to a struct of a specified type.
+// It first marshals the interface{} to JSON bytes and
+// then unmarshals those bytes into the struct.
+func MapInterfaceToStruct[T any](data interface{}) (*T, error) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal interface to JSON: %w", err)
+	}
+
+	// Initialize the generic type
+	var v T
+
+	// Unmarshal the JSON bytes into the struct
+	if err := json.Unmarshal(jsonData, &v); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON to PriceSettings: %w", err)
+	}
+	return &v, nil
 }
