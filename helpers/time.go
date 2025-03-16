49b0419d3a89148cbd85dc3da9802479bc938e39
anhcao <anhcao4922@gmail.com>
2025-03-16 11:23:57 +0200

    feat: add common functions for Time matter

--- helpers/time.go
+++ helpers/time.go
@@ -5,9 +5,12 @@ import (
 	"time"
 )
 
-// SetTime sets the time to the specified hour and minute in the Helsinki timezone
-// and returns the corresponding time in UTC.
-//
+const (
+	DATE_FORMAT    string = "2006-01-02"          // this is just the layout of YYYY-MM-DD
+	UI_DATE_FORMAT string = "2006-01-02 15:04:05" // this is just the layout of YYYY-MM-DD HH:MM:SS
+)
+
+// SetTime returns current date based on application's time with specific hour and minute.
 // Parameters:
 //   - hour: The hour to set (0-23).
 //   - minute: The minute to set (0-59).
@@ -16,58 +19,38 @@ import (
 //   - time.Time: The time set to the specified hour and minute in UTC.
 //   - error: An error if the Helsinki timezone could not be loaded.
 func SetTime(hour int, minute int) (time.Time, error) {
-	// get timezone
-	location, err := loadHelsinkiLocation()
-	if err != nil {
-		return time.Now(), err
-	}
-
-	// Get current time in Finnish time
-	now := time.Now().In(location)
-
-	// Get year, month, and day components
-	year, month, day := now.Date()
-	settingTime := time.Date(year, month, day, hour, minute, 0, 0, location)
-	// return as UTC
-	return settingTime.UTC(), nil
+	year, month, day := time.Now().Date()
+	settingTime := time.Date(year, month, day, hour, minute, 0, 0, time.Local)
+	return settingTime, nil
 }
 
-// Get current time in Finnish time then convert to UTC
-func GetCurrentTimeInUTC() (time.Time, error) {
-	location, err := loadHelsinkiLocation()
-	if err != nil {
-		return time.Now(), err
-	}
-	now := time.Now().In(location)
-
-	// Get year, month, and day components
-	year, month, day := now.Date()
-	currentTime := time.Date(year, month, day, now.Hour(), now.Minute(), 0, 0, location)
-	// return as UTC
-	return currentTime.UTC(), nil
-}
-
-// GetCurrentTimeInHelsinki returns the current time in Helsinki, Finland.
-// It loads the Helsinki location and adjusts the current time to that timezone.
-// The function returns the current time in Helsinki, the location object, and an error if any occurred during the location loading process.
-func GetCurrentTimeInHelsinki() (time.Time, *time.Location, error) {
-	location, err := loadHelsinkiLocation()
-	if err != nil {
-		return time.Now(), nil, err
-	}
-	now := time.Now().In(location)
-
-	// Get year, month, and day components
-	year, month, day := now.Date()
-	currentTime := time.Date(year, month, day, now.Hour(), now.Minute(), 0, 0, location)
-	// return as UTC
-	return currentTime, location, nil
-}
-
-func loadHelsinkiLocation() (*time.Location, error) {
+// LoadHelsinkiLocation loads the time.Location for Helsinki, Finland.
+// It returns a pointer to the time.Location and an error if the location
+// could not be loaded.
+func LoadHelsinkiLocation() (*time.Location, error) {
 	location, err := time.LoadLocation("Europe/Helsinki")
 	if err != nil {
 		return nil, fmt.Errorf("failed to get current location: %s", err.Error())
 	}
 	return location, nil
+}
+
+// getTodayDate returns date of today in "YYYY-MM-DD" format
+func GetTodayDate() string {
+	today := time.Now()
+	return today.Format(DATE_FORMAT)
+}
+
+// GetTomorrowDate returns date of tomorrow in "YYYY-MM-DD" format
+func GetTomorrowDate() string {
+	today := time.Now()
+	tomorrow := today.AddDate(0, 0, 1)
+	return tomorrow.Format(DATE_FORMAT)
+}
+
+// getYesterdayDate returns date of yesterday in "YYYY-MM-DD" format
+func GetYesterdayDate() string {
+	today := time.Now()
+	yesterday := today.AddDate(0, 0, -1)
+	return yesterday.Format(DATE_FORMAT)
 }
