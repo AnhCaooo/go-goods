e04187dc179ef1fdf2115aaffc3bf052a26c128e
anhcao <anhcao4922@gmail.com>
2025-08-31 17:42:07 +0300

    feat: create env package and make logger more dynamically
    Signed-off-by: anhcao <anhcao4922@gmail.com>

--- logger/logger.go
+++ logger/logger.go
@@ -4,35 +4,48 @@ package logger
 import (
 	"time"
 
+	"github.com/AnhCaooo/go-goods/env"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 /*
-initialize and return logger instance (unstructured log). After initialization, use logger as dependency injection
+initialize and return logger instance (unstructured log). After initialization, use logger as dependency injection.
+By default, log level is info. To set the log level, pass environment variable LOG_LEVEL with value DEBUG
 
 *Note*: it is recommended to buffered log entries are flushed before the program exits.
 
 Example usage:
 
-	logger := log.InitLogger(zapcore.InfoLevel)
+	logger := log.InitLogger()
 	defer logger.Sync()
 */
-func InitLogger(level zapcore.Level, location *time.Location) *zap.Logger {
-	// todo: do we want to store the log in specific file for investigation purposes?
+func InitLogger(location *time.Location) *zap.Logger {
 	cfg := zap.NewProductionConfig()
 	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 	cfg.EncoderConfig.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 		enc.AppendString(t.In(location).Format("2006-01-02 15:04:05"))
 	}
+
 	// Set log level
-	cfg.Level.SetLevel(zapcore.Level(level))
+	level := getLogLevel()
+	cfg.Level.SetLevel(zapcore.Level(*level))
 	// Disable JSON encoding
 	cfg.Encoding = "console"
-
 	logger, err := cfg.Build()
 	if err != nil {
 		panic(err)
 	}
 	return logger
+}
+
+func getLogLevel() *zapcore.Level {
+	var logLevel zapcore.Level
+	switch env.LogLevel.GetValue() {
+	case "DEBUG":
+		logLevel = zapcore.DebugLevel
+	default:
+		logLevel = zapcore.InfoLevel
+	}
+	return &logLevel
 }
